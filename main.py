076a506f3c464a47142bc5122966ec6c02c3a2cf
Alexandr German <lego0sasha2012@gmail.com>
2017-06-05 16:37:24 +0000

    AW SHIT ALMOST DONE BOIII

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ if __name__=="__main__":
            module_name = " ".join(args)
            if module_name in modloader.DISABLED or module_name in modloader.FAILURE:
                update.effective_message.reply_text("Module is disabled")
-            elif module_name in modloader.ENABLED:
-                modloader.ENABLED.get(module_name).help(update.effective_message, [])
+           elif module_name in modloader.ENABLED:
+               modloader.ENABLED.get(module_name).help(update.effective_message, [])
            else:
                update.effective_message.reply_text("Module not found")
 
@@ -51,20 +51,7 @@ if __name__=="__main__":
         disabled_modlist = ''
         for module_name in sorted(modloader.DISABLED):
             disabled_modlist += ' - {0}\n'.format(module_name)
-        update.effective_message.reply_text(tr(lang, 'modules').format(modlist, fail_modlist,
-                                                                   disabled_modlist),
-                                        parse_mode="HTML")
-
-
-    def module_list(bot, update):
-        lang = update.effective_user.language_code
-        modlist = ''
-        for module_name in modloader.SUCCESS:
-            modlist += f' - {module_name}\n'
-        fail_modlist = ''
-        for (module_name, e) in modloader.FAILURE.items():
-            fail_modlist += f' - {module_name}: {e}\n'
-        update.effective_message.reply_text(tr(lang, 'modules').format(modlist, fail_modlist),
+        update.effective_message.reply_text(tr(lang, 'modules').format(modlist, fail_modlist, disabled_modlist),
                                             parse_mode="HTML")
 
 
--- modules/multi_spin.py
+++ modules/multi_spin.py
@@ -1,6 +1,5 @@
 # Kostilniy modul
 from bases import BaseTelegramModule
-from main import dp
 from telegram import (Bot, Update)
 from telegram.ext import (Filters, MessageHandler)
 from telegram.utils.helpers import escape_markdown
@@ -18,13 +17,17 @@ class TelegramModule(BaseTelegramModule):
             pickle.dump(obj, ff, pickle.HIGHEST_PROTOCOL)
 
 
+    def _is_private(self, chat_id):
+        return chat_id > 0
+
+
     def __init__(self, api):
         super(TelegramModule, self).__init__(api)
         self.friendly_name = "Multi Spin"
+        self._telegram_api._dispatcher.add_handler(MessageHandler(Filters.all, self.update_cache, edited_updates=True),
+                       group=-1)
         self._telegram_api.register_command(['spin'], self.spin)
         self._telegram_api.register_command(['spin_setname'], self.spin_setname)
-        dp.add_handler(MessageHandler(Filters.all, self.update_cache, edited_updates=True),
-                       group=-1)
         self.spin_name = self._load("spins.pkl")
         self.results_today = {}
         self.chat_users = self._load("chat.pkl")
@@ -34,18 +37,19 @@ class TelegramModule(BaseTelegramModule):
     def spin_setname(self, message, args):
         #pass
         chat_id = message.chat_id
+        print(args)
         try:
-            spin_num = int(args.split()[0])
+            spin_num = int(args[0])
         except Exception:
             self._telegram_api.send_text_message(chat_id,
                                                  "Usage `/spin_setname <num> <name>`",
                                                  markdown=True)
             return
         try:
-            self.spin_name[chat_id][spin_num].update(args[len(args.split()[0])+1:])
+            self.spin_name[chat_id][spin_num].update(args[1])
         except Exception:
             self.spin_name[chat_id] = {}
-            self.spin_name[chat_id][spin_num].update(args[len(args.split()[0])+1:])
+            self.spin_name[chat_id].update({spin_num: args[1]})
         self._telegram_api.send_text_message(message.chat_id, "*Done*", markdown=True)
 
 
@@ -55,12 +59,13 @@ class TelegramModule(BaseTelegramModule):
             spin_num = int(args[0])
         except Exception:
             #self._telegram_api.send_text_message(chat_id, "Gimme int dumbass")
-            choice(self.spin_name.get(chat_id, {"79|0ub0d0u9|"}))
-            return
-        s = escape_markdown(self.spin_name[chat_id].get(spin_num))
+            spin_num = choice(list(self.spin_name.get(chat_id, [0])))
+        print(self.spin_name[chat_id].get(spin_num, "79|0ub0d0u9|"))
+        s = escape_markdown(self.spin_name[chat_id].get(spin_num, "79|0ub0d0u9|"))
         try:
             p = self.results_today[chat_id].get(spin_num)
         except Exception:
+            p = ""
             p = None
             self.results_today[chat_id] = {}
         if chat_id in self.locks:
@@ -69,21 +74,32 @@ class TelegramModule(BaseTelegramModule):
             self._telegram_api.send_text_message(chat_id, self.TEXT_ALREADY.format(s=s, n=p),
                                                  markdown=True)
         else:
-            p = escape_markdown(self.choose_random_user(chat_id))
+            p = escape_markdown(self.choose_random_user(chat_id, spin_num))
             from time import sleep
             curr_text = choice(self.TEXTS)
             self.locks.append(chat_id)
             for t in curr_text:
-                self._telegram_api.send_message(chat_id, self.t.format(s=s, n=p),
+                self._telegram_api.send_message(chat_id, t.format(s=s, n=p),
                                                 mardown=True)
                 sleep(2)
         self.locks.pop(locks.index(chat_id))
 
 
     def update_cache(self, bot: Bot, update: Update):
+        print("IM WORKING")
         user = update.effective_user
         chat_id = update.effective_message.chat_id
         # Also skip first update when the bot is added
-        if not _is_private(chat_id) and self.chat_users.get(chat_id) is not None:
-            self.chat_users[chat_id].update({user.id: user.name})
+        if not self._is_private(chat_id):
+            try:
+                self.chat_users[chat_id].update({user.id: user.name})
+            except KeyError:
+                self.chat_users[chat_id]={}
+                self.chat_users[chat_id].update({user.id: user.name})
 
+
+    def choose_random_user(self, chat_id, spin_num):
+        print(self.chat_users[chat_id])
+        i = choice(list(self.chat_users[chat_id]))
+        self.results_today[chat_id][spin_num] = self.chat_users[chat_id][i]
+        return self.chat_users[chat_id][i]
