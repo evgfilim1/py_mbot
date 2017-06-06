1a72e4a03d241528fedf42fa10c85957f4fe5e4f
Ivan Pakhomov <sojaalei@gmail.com>
2017-06-06 21:42:26 +0500

    add new command

--- modules/textmemes.py
+++ modules/textmemes.py
@@ -1,17 +1,34 @@
 from bases import BaseTelegramModule
+from random import shuffle
+
+FACES_LIST = ["ლ(ಠ益ಠლ)", "/╲/\╭( ͡° ͡° ͜ʖ ͡° ͡°)╮/\╱\\", "(;´༎ຶД༎ຶ`)", "♪~ ᕕ(ᐛ)ᕗ", "♥‿♥",
+              "▄︻̷̿┻̿═━一", "( ͡°( ͡° ͜ʖ( ͡° ͜ʖ ͡°)ʖ ͡°) ͡°)", "ʕ•ᴥ•ʔ", "(▀̿Ĺ̯▀̿ ̿)", "(ง ͠° ͟ل͜ ͡°)ง",
+              "༼ つ ◕_◕ ༽つ", "(づ｡◕‿‿◕｡)づ", "(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ ✧ﾟ･: *ヽ(◕ヮ◕ヽ)", "[̲̅$̲̅(̲̅5̲̅)̲̅$̲̅]",
+              "┬┴┬┴┤ ͜ʖ ͡°) ├┬┴┬┴", "( ͡°╭͜ʖ╮͡° )", "(͡ ͡° ͜ つ ͡͡°)", "(• ε •)", "(ง'̀-'́)ง", "(ಥ﹏ಥ)",
+              "﴾͡๏̯͡๏﴿ O'RLY?", "(ノಠ益ಠ)ノ彡┻━┻", "[̲̅$̲̅(̲̅ ͡° ͜ʖ ͡°̲̅)̲̅$̲̅]", "(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧", "(☞ﾟ∀ﾟ)☞",
+              "| (• ◡•)| (❍ᴥ❍ʋ)", "(◕‿◕✿)", "(ᵔᴥᵔ)", "(¬‿¬)", "(☞ﾟヮﾟ)☞ ☜(ﾟヮﾟ☜)", "(づ￣ ³￣)づ",
+              "(~˘▾˘)~", "(._.) ( l: ) ( .-. ) ( :l ) (._.)", "༼ʘ̚ل͜ʘ̚༽", "༼ ºل͟º ༼ ºل͟º ༼ ºل͟º ༽ ºل͟º ༽ ºل͟º ༽",
+              "༼ つ  ͡° ͜ʖ ͡° ༽つ", "(╯°□°）╯︵ ┻━┻", "( ͡ᵔ ͜ʖ ͡ᵔ )", "ヾ(⌐■_■)ノ♪", "~(˘▾˘~)", "◉_◉", "\ (•◡•) /",
+              "┬┴┬┴┤(･_├┬┴┬┴", "ᕙ(⇀‸↼‶)ᕗ", "ᕦ(ò_óˇ)ᕤ", "┻━┻ ︵ヽ(`Д´)ﾉ︵ ┻━┻", "⚆ _ ⚆", "(•_•) ( •_•)>⌐■-■ (⌐■_■)",
+              "(｡◕‿‿◕｡)", "ヽ༼ຈل͜ຈ༽ﾉ", "(☞ຈل͜ຈ)☞", "☜(˚▽˚)☞", "˙ ͜ʟ˙", "(｡◕‿◕｡)", "（╯°□°）╯︵( .o.)", ":')", "(°ロ°)☝",
+              "┬──┬ ノ( ゜-゜ノ)", "（╯°□°）╯︵( .o.)", "(っ˘ڡ˘ς)", "ლ(´ڡ`ლ)", "｡◕‿‿◕｡", "(/) (°,,°) (/)", "☼.☼", "^̮^"]
 
 
 class TelegramModule(BaseTelegramModule):
     def __init__(self, telegram_api):
         super(TelegramModule, self).__init__(telegram_api)
-        self.commands = ((['lenny'], self.lenny), (['shrug'], self.shrug))
+        self.commands = ((['lenny'], self.lenny), (['shrug'], self.shrug), (['random_face'], self.random_face))
         self._register_module()
 
     def help(self, message, args):
-        self._telegram_api.send_text_message(message.chat_id, "This module sends /shrug or /lenny")
+        self._telegram_api.send_text_message(message.chat_id, "This module sends /shrug, /lenny or /random_face")
 
     def lenny(self, message, args):
         self._telegram_api.send_text_message(message.chat_id, "( ͡° ͜ʖ ͡° )")
 
     def shrug(self, message, args):
         self._telegram_api.send_text_message(message.chat_id, "¯\_(ツ)_/¯")
+
+    def random_face(self, message, args):
+        shuffle(FACES_LIST)
+        self._telegram_api.send_text_message(message.chat_id, FACES_LIST[1])
