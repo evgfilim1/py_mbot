7f9b116e471694037d6f91917b685fa1cb4b2d67
Ivan Pakhomov <sojaalei@gmail.com>
2017-06-06 22:32:34 +0500

    fix for pep8

--- modules/textmemes.py
+++ modules/textmemes.py
@@ -20,11 +20,14 @@ FACES_LIST = ["ლ(ಠ益ಠლ)", "/╲/\╭( ͡° ͡° ͜ʖ ͡° ͡°)╮/\╱
 class TelegramModule(BaseTelegramModule):
     def __init__(self, telegram_api):
         super(TelegramModule, self).__init__(telegram_api)
-        self.commands = ((['lenny'], self.lenny), (['shrug'], self.shrug), (['random_face'], self.random_face))
+        self.commands = ((['lenny'], self.lenny),
+                         (['shrug'], self.shrug),
+                         (['random_face'], self.random_face))
         self._register_module()
 
     def help(self, message, args):
-        self._telegram_api.send_text_message(message.chat_id, "This module sends /shrug, /lenny or /random_face")
+        self._telegram_api.send_text_message(message.chat_id, "This module sends /shrug, \
+                                             /lenny or /random_face")
 
     def lenny(self, message, args):
         self._telegram_api.send_text_message(message.chat_id, "( ͡° ͜ʖ ͡° )")
